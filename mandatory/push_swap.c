964535e09e51d7aec67eff1620ee6f3347e3f42c
ayats37 <ayatouirass27@gmail.com>
2025-03-03 04:53:52 +0000

    esfjkesg

--- mandatory/push_swap.c
+++ mandatory/push_swap.c
@@ -6,24 +6,51 @@
 /*   By: taya <taya@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 12:59:24 by taya              #+#    #+#             */
-/*   Updated: 2025/03/03 02:54:05 by taya             ###   ########.fr       */
+/*   Updated: 2025/03/03 04:41:20 by taya             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// bool is_sorted(t_stack *stack) {
-//     if (stack->size <= 1)
-//         return true;
-    
-//     t_node *current = stack->head;
-//     while (current->next) {
-//         if (current->value > current->next->value)
-//             return false;
-//         current = current->next;
-//     }
-//     return true;
-// }
+int is_digit(int c) 
+{
+    return (c >= '0' && c <= '9');
+}
+int valid_nbr(const char *str)
+{
+	int i;
+	
+	i = 0;
+	if (str[0] == '-' || str[0] == '+')
+		i = 1;
+	while (str[i])
+	{
+		if (!is_digit(str[i]))
+			return (0);
+		i++;
+	}
+	return(1);
+}
+int	duplicate(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
+				return(1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+ 
 void init_stack(t_stack *stack) {
     stack->head = NULL;
     stack->tail = NULL;
@@ -49,6 +76,21 @@ int main(int argc, char **argv)
 	int i;
 	int num;
 
+	if (argc < 2 || duplicate(argc, argv))
+	{
+		write(1, "Error\n", 6);
+		return (1);
+	}
+	i = 1;
+	while(i < argc)
+	{
+		if (!valid_nbr(argv[i]))
+		{
+			write(1, "Error\n", 6);
+			return (1);
+		}
+		i++;
+	}
 	init_stack(&stack_a);
 	init_stack(&stack_b);
 	i = argc - 1;
